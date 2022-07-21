15399428ec07f0c99f599ba9e7edfb3168e54751
Erik <Erik.quintana@icloud.com>
2022-07-21 11:39:52 -0600

    CRUD inst

--- app/controllers/instructors_controller.rb
+++ app/controllers/instructors_controller.rb
@@ -5,15 +5,38 @@ class InstructorsController < ApplicationController
   end
 
   def create
+  instructor = Instructor.create(name: params[:name])
+  render json: instructor, status: :created 
 
   end
 
   def update
+    instructor = Instructor.find_by(id: params[:id])
+    if instructor
+      instructor.update(instructor_params)
+      render json: instructor
+    else
+      render json: {error: "Instructor is not found"}, status: :not_found
+    end
 
   end
 
   def destroy
+    instructor = Instructor.find_by(id: params[:id])
+    if instructor
+      instructor.destroy 
+      head :no_content
+    else
+      render json: {error: "Instructor is not found"}, status: :not_found
+    
+    end
 
+  end
+
+  private
+
+  def instructor_params
+    params.permit(:name)
   end
 
 end
